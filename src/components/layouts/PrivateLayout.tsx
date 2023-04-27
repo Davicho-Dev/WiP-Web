e68c3991a70d5d387d265dc5f73eec754c854f7d
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-04-27 07:55:59 -0500

    CHANGE: Private layout ButtonSolid to NavLink;
    Signed-off-by: DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>

--- src/components/layouts/PrivateLayout.tsx
+++ src/components/layouts/PrivateLayout.tsx
@@ -1,41 +1,49 @@
 import { Outlet, useNavigate } from 'react-router-dom'
-import { ButtonSolid } from '../atoms'
+
+import { useAppSelector } from '../../hooks'
+import { ButtonNavLink, ButtonSolid } from '../atoms'
+import { IcLogo } from '../atoms/Icons'
 
 const PrivateLayout = () => {
 	const navigate = useNavigate()
 
+	const { username, picture } = useAppSelector(state => state.user)
+
 	return (
-		<section className='w-screen h-screen grid grid-flow-col grid-cols-[15rem,_1fr]'>
-			<aside className='bg-[#f9fbf1] border-r border-r-neutral-300 py-5 px-3'>
-				<nav className='flex flex-col gap-y-6'>
-					<ButtonSolid
-						type='button'
-						className='w-full pl-4 bg-neutral-900 text-secondary text-left'
-						label='Home_'
-						onClick={() => navigate('/settings')}
-					/>
-					<ButtonSolid
-						type='button'
-						className='w-full pl-4 bg-transparent text-neutral-900 text-left border-2 border-neutral-900'
-						label='Direct messages_'
-						onClick={() => navigate('/settings')}
-					/>
-					<ButtonSolid
-						type='button'
-						className='w-full pl-4 bg-transparent text-neutral-900 text-left border-2 border-neutral-900'
-						label='Post you like_'
-						onClick={() => navigate('/settings')}
-					/>
-					<ButtonSolid
-						type='button'
-						className='w-full pl-4 bg-primary text-white text-left'
-						label='Create_'
-						onClick={() => navigate('/settings')}
-					/>
+		<main className='w-screen h-screen flex flex-col shrink-0'>
+			<header className='w-full bg-neutral-800 flex justify-between py-4 px-10'>
+				<IcLogo className='h-8 fill-secondary' />
+				<nav className='flex justify-between items-center'>
+					<figure
+						className='w-7 h-7 rounded-full overflow-hidden cursor-pointer'
+						onClick={() => navigate('/user')}
+					>
+						<img
+							className='w-full h-full object-cover'
+							src={picture}
+							alt={username + ' Avatar'}
+							title={username + ' Avatar'}
+						/>
+					</figure>
 				</nav>
-			</aside>
-			<Outlet />
-		</section>
+			</header>
+			<section className='w-full h-full grid grid-flow-col grid-cols-[15rem,_1fr] grow'>
+				<aside className='bg-[#f9fbf1] border-r border-r-neutral-300 py-5 px-3'>
+					<nav className='h-full flex flex-col gap-y-6'>
+						<ButtonNavLink to='/' label='Home_' />
+						<ButtonNavLink to='/settings' label='Post you like_' />
+						<ButtonNavLink to='/settings' label='Direct messages_' />
+						<ButtonSolid
+							type='button'
+							className='w-full pl-4 bg-primary text-white text-left'
+							label='Create_'
+							onClick={() => navigate('/settings')}
+						/>
+					</nav>
+				</aside>
+				<Outlet />
+			</section>
+		</main>
 	)
 }
 
