34ccb5b71f3285be44c9cf3fbb35e6c265612790
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-05-05 09:20:23 -0500

    ADD: '+' symbol in regex
    Signed-off-by: DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,4 +1,4 @@
-const PATTERN_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/gm
+const PATTERN_EMAIL = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/gm
 const PATTERN_PASSWORD =
 	/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-])/gm
 const PATTERN_PHONE = /^\+[1-9]{1}[0-9]{3,14}$/
