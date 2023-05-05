4aa328b22b12a0b70804990c2399e0448dfd3972
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-05-05 10:07:49 -0500

    FIX: Login legend;
    Signed-off-by: DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>

--- src/components/layouts/PublicLayout/PublicLayout.tsx
+++ src/components/layouts/PublicLayout/PublicLayout.tsx
@@ -4,7 +4,7 @@ const PublicLayout = () => {
 	return (
 		<section className='w-screen h-screen flex justify-center lg:!justify-end items-center relative bg-mobile bg-cover lg:!bg-web lg:!bg-contain'>
 			<h1 className='w-40 top-14 left-40 text-3xl absolute hidden lg:!inline'>
-				LET’S PUSH THINGS FORWARD_
+				ALL ART IS A WORK IN PROGRESS_
 			</h1>
 			<Outlet />
 		</section>
