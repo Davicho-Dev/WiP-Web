c109523652a25dd75bf34e28bb2349fd120b6176
DavichoDev <13292121+Davicho-Dev@users.noreply.github.com>
2023-05-07 15:19:52 -0500

    ADD: Unknown query error toast;

--- src/helpers/hdlErrors.ts
+++ src/helpers/hdlErrors.ts
@@ -3,6 +3,12 @@ import { toast } from 'react-toastify'
 
 export const hdlErrors = (err: AxiosError) => {
 	const { response } = err
+	console.log(err)
+
+	if (!response || !response.data) {
+		toast.error('Network Error')
+		return
+	}
 
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
